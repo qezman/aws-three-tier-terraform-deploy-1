228056bcb376790f5312a6c40935f628597b0641
Kazeem Jimoh <holaryinka5050@gmail.com>
2026-05-25 18:41:28 +0100

    temp: comment out dns module while cluster rebuilds

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
-output "route53_name_servers" {
-  description = "Route53 nameservers to add to Namecheap"
-  value       = module.namecheap-deployment.route53_name_servers
-}
+# output "route53_name_servers" {
+#   description = "Route53 nameservers to add to Namecheap"
+#   value       = module.namecheap-deployment.route53_name_servers
+# }
