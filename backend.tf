b5f420ed807e0bca10e49678a379015194741e8c
Kazeem Jimoh <holaryinka5050@gmail.com>
2026-05-25 13:37:40 +0100

    completed code

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "digitalwitchngbucketcloud1"
-    key    = "digitalwitchng/prodution/terraform.tfstate"
-    region = "us-west-1"
+    bucket = "qezman-terraform-state"
+    key = "aws-three-tier/production/terraform.tfstate"
+    region = "us-east-1"
   }
 }
