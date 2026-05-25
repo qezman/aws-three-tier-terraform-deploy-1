204206932d2257156aa9800c72260583bd406d12
Kazeem Jimoh <holaryinka5050@gmail.com>
2026-05-25 18:44:44 +0100

    temp: disable helm/k8s providers for clean cluster rebuild

--- module-eks/addons.tf
+++ module-eks/addons.tf
@@ -1,21 +1,21 @@
-provider "helm" {
-  kubernetes {
-    host                   = aws_eks_cluster.eks.endpoint
-    cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.eks.token
-  }
-}
+# provider "helm" {
+#   kubernetes {
+#     host                   = aws_eks_cluster.eks.endpoint
+#     cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
+#     token                  = data.aws_eks_cluster_auth.eks.token
+#   }
+# }
 
-provider "kubernetes" {
-  alias                  = "eks"
-  host                   = aws_eks_cluster.eks.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.eks.token
-}
+# provider "kubernetes" {
+#   alias                  = "eks"
+#   host                   = aws_eks_cluster.eks.endpoint
+#   cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
+#   token                  = data.aws_eks_cluster_auth.eks.token
+# }
 
-data "aws_eks_cluster_auth" "eks" {
-  name = aws_eks_cluster.eks.name
-}
+# data "aws_eks_cluster_auth" "eks" {
+#   name = aws_eks_cluster.eks.name
+# }
 # resource "helm_release" "nginx_ingress" {
 #   name             = "nginx-ingress"
 #   repository       = "https://kubernetes.github.io/ingress-nginx"
