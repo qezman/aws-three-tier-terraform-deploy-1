2fcbc9b8258447ef478913d54d38cd10dc6f50c9
Kazeem Jimoh <holaryinka5050@gmail.com>
2026-05-25 18:31:35 +0100

    temp: disable helm releases while cluster rebuilds

--- module-eks/addons.tf
+++ module-eks/addons.tf
@@ -16,54 +16,54 @@ provider "kubernetes" {
 data "aws_eks_cluster_auth" "eks" {
   name = aws_eks_cluster.eks.name
 }
-resource "helm_release" "nginx_ingress" {
-  name             = "nginx-ingress"
-  repository       = "https://kubernetes.github.io/ingress-nginx"
-  chart            = "ingress-nginx"
-  version          = "4.12.0"
-  namespace        = "ingress-nginx"
-  create_namespace = true
-  timeout          = 600
-  wait             = true
+# resource "helm_release" "nginx_ingress" {
+#   name             = "nginx-ingress"
+#   repository       = "https://kubernetes.github.io/ingress-nginx"
+#   chart            = "ingress-nginx"
+#   version          = "4.12.0"
+#   namespace        = "ingress-nginx"
+#   create_namespace = true
+#   timeout          = 600
+#   wait             = true
 
-  values     = [file("${path.module}/nginx-ingress-values.yaml")]
-  depends_on = [aws_eks_node_group.eks_node_group]
-}
+#   values     = [file("${path.module}/nginx-ingress-values.yaml")]
+#   depends_on = [aws_eks_node_group.eks_node_group]
+# }
 
-data "aws_lb" "nginx_ingress" {
-  tags = {
-    "kubernetes.io/service-name" = "ingress-nginx/nginx-ingress-ingress-nginx-controller"
-  }
+# data "aws_lb" "nginx_ingress" {
+#   tags = {
+#     "kubernetes.io/service-name" = "ingress-nginx/nginx-ingress-ingress-nginx-controller"
+#   }
 
-  depends_on = [helm_release.nginx_ingress]
-}
+#   depends_on = [helm_release.nginx_ingress]
+# }
 
-resource "helm_release" "cert_manager" {
-  name             = "cert-manager"
-  repository       = "https://charts.jetstack.io"
-  chart            = "cert-manager"
-  version          = "1.14.5"
-  namespace        = "cert-manager"
-  create_namespace = true
-  timeout          = 600
-  wait             = true
-  set {
-    name  = "installCRDs"
-    value = "true"
-  }
-  depends_on = [helm_release.nginx_ingress]
-}
+# resource "helm_release" "cert_manager" {
+#   name             = "cert-manager"
+#   repository       = "https://charts.jetstack.io"
+#   chart            = "cert-manager"
+#   version          = "1.14.5"
+#   namespace        = "cert-manager"
+#   create_namespace = true
+#   timeout          = 600
+#   wait             = true
+#   set {
+#     name  = "installCRDs"
+#     value = "true"
+#   }
+#   depends_on = [helm_release.nginx_ingress]
+# }
 #==================================================
 
-resource "helm_release" "argocd" {
-  name             = "argocd"
-  repository       = "https://argoproj.github.io/argo-helm"
-  chart            = "argo-cd"
-  version          = "5.51.6"
-  namespace        = "argocd"
-  create_namespace = true
-  timeout          = 600
-  wait             = true
-  values           = [file("${path.module}/argocd-values.yaml")]
-  depends_on       = [helm_release.nginx_ingress, helm_release.cert_manager]
-}
+# resource "helm_release" "argocd" {
+#   name             = "argocd"
+#   repository       = "https://argoproj.github.io/argo-helm"
+#   chart            = "argo-cd"
+#   version          = "5.51.6"
+#   namespace        = "argocd"
+#   create_namespace = true
+#   timeout          = 600
+#   wait             = true
+#   values           = [file("${path.module}/argocd-values.yaml")]
+#   depends_on       = [helm_release.nginx_ingress, helm_release.cert_manager]
+# }
